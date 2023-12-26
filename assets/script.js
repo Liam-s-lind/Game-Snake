60053d5ba9832e7cb987c12f242698527bb45ec8
Liam-s-lind <>
2023-12-26 14:21:10 +0000

    movement for snake

--- assets/script.js
+++ assets/script.js
@@ -75,3 +75,23 @@ function update() {
         }
     }
 }
+
+// movement for snake
+function changeDirection(e) {
+    if (e.code == "ArrowUp" && velocityY != 1) {
+        velocityX = 0;
+        velocityY = -1;
+    }
+    else if (e.code == "ArrowDown" && velocityY != -1) {
+        velocityX = 0;
+        velocityY = 1;
+    }
+    else if (e.code == "ArrowLeft" && velocityX != 1) {
+        velocityX = -1;
+        velocityY = 0;
+    }
+    else if (e.code == "ArrowRight" && velocityX != -1) {
+        velocityX = 1;
+        velocityY = 0;
+    }
+}
