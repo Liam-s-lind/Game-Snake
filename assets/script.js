62928f990998dc664d6a0253ecce6a02b7d60398
Liam-s-lind <>
2023-12-26 14:19:41 +0000

    added functions and conditions

--- assets/script.js
+++ assets/script.js
@@ -30,3 +30,48 @@ window.onload = function () {
     document.addEventListener("keyup", changeDirection);
     setInterval(update, 1000 / 10); // update placement in canvas every 100 milliseconds
 };
+
+function update() {
+    if (gameOver) {
+        return;
+    }
+    context.fillStyle = "black"; //gives color to the bord
+    context.fillRect(0, 0, board.height, board.width); // this make it start filling the color from point 0,0 of the board
+
+    context.fillStyle = "red"; //color for the food
+    context.fillRect(foodX, foodY, blockSize, blockSize); // place for the snakes food
+
+    if (snakeX == foodX && snakeY == foodY) {
+        snakeBody.push([foodX, foodY]);
+        placeFood();
+    }
+    //body moving along with head
+    for (let i = snakeBody.length - 1; i > 0; i--) {
+        snakeBody[i] = snakeBody[i - 1];
+    }
+    if (snakeBody.length) {
+        snakeBody[0] = [snakeX, snakeY];
+    }
+
+    context.fillStyle = "blue"; // color for the snake
+    snakeX += velocityX * blockSize; // making snake speed X axis faster
+    snakeY += velocityY * blockSize; // -:- but Y axis
+    context.fillRect(snakeX, snakeY, blockSize, blockSize); // place for the snake
+
+    for (let i = 0; i < snakeBody.length; i++) {
+        context.fillRect(snakeBody[i][0], snakeBody[i][1], blockSize, blockSize);
+    }
+
+    //Condition for game over
+    if (snakeX < 0 || snakeX > cols * blockSize || snakeY < 0 || snakeY > rows * blockSize) {
+        gameOver = true;
+        showGameOverAlert();
+    }
+
+    for (let i = 0; i < snakeBody.length; i++) {
+        if (snakeX == snakeBody[i][0] && snakeY == snakeBody[i][1]) {
+            gameOver = true;
+            showGameOverAlert();
+        }
+    }
+}
