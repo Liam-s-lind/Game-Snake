391dbc7df6b986f40166ca682d2e5a701cc14a64
Liam-s-lind <>
2023-12-26 14:12:07 +0000

    added onload

--- assets/script.js
+++ assets/script.js
@@ -19,3 +19,14 @@ var foodX;
 var foodY;
 
 var gameOver = false;
+
+window.onload = function () {
+    board = document.getElementById("board");
+    board.height = rows * blockSize;
+    board.width = cols * blockSize;
+    context = board.getContext("2d"); //this is used for the drawing on the board
+
+    placeFood();
+    document.addEventListener("keyup", changeDirection);
+    setInterval(update, 1000 / 10); // update placement in canvas every 100 milliseconds
+};
