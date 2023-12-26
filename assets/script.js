3891c68258bf00b3d8ef8d386e3a2994cb61c55d
Liam-s-lind <>
2023-12-26 14:09:34 +0000

    added rest of variables

--- assets/script.js
+++ assets/script.js
@@ -4,3 +4,18 @@ var rows = 20;
 var cols = 20;
 var board;
 var context;
+
+//the snakes head
+var snakeX = blockSize * 5;
+var snakeY = blockSize * 5;
+
+var velocityX = 0;
+var velocityY = 0;
+
+var snakeBody = [];
+
+//the snakes food
+var foodX;
+var foodY;
+
+var gameOver = false;
